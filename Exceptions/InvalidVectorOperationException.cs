c0c4bf99b3767ff23a580afd3d1521aab86441ce
Sachintha Silva <netsachintha@gmail.com>
2022-04-16 18:35:14 +0530

    Update code documentation and code summary description

--- Exceptions/InvalidVectorOperationException.cs
+++ Exceptions/InvalidVectorOperationException.cs
@@ -1,23 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VectorLibrary.Exceptions
 {
+    /// <summary>
+    /// Exception when the vector is unable to perform the iven operation.
+    /// </summary>
     public class InvalidVectorOperationException : Exception
     {
+        /// <summary>
+        /// Initialize an instance of InvalidVectorOperationException.
+        /// </summary>
         public InvalidVectorOperationException()
         {
 
         }
 
+        /// <summary>
+        /// Initialize an instance of InvalidVectorOperationException with specified message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for this exception.</param>
         public InvalidVectorOperationException(string message) : base(message)
         {
 
         }
 
+        /// <summary>
+        /// Initialize an instance of InvalidVectorOperationException with specified message and exception that cause this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for this exception.</param>
+        /// <param name="inner">The exception that is the cause of the current exception, or a null reference.</param>
         public InvalidVectorOperationException(string message, Exception inner) : base(message, inner)
         {
 
--- Exceptions/VectorNotInitializedException.cs
+++ Exceptions/VectorNotInitializedException.cs
@@ -1,23 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VectorLibrary.Exceptions
 {
+    /// <summary>
+    /// Exception when the vector is not initialized.
+    /// </summary>
     public class VectorNotInitializedException : Exception
     {
+        /// <summary>
+        /// Initialize an instance of VectorNotInitializedException.
+        /// </summary>
         public VectorNotInitializedException()
         {
 
         }
 
+        /// <summary>
+        /// Initialize an instance of VectorNotInitializedException with specified message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for this exception.</param>
         public VectorNotInitializedException(string message) : base(message)
         {
 
         }
 
+        /// <summary>
+        /// Initialize an instance of VectorNotInitializedException with specified message and exception that cause this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for this exception.</param>
+        /// <param name="inner">The exception that is the cause of the current exception, or a null reference.</param>
         public VectorNotInitializedException(string message, Exception inner) : base(message, inner)
         {
 
